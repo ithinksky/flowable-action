cd5e8c8258c43ec56c7ff7d7918167f51717750a
tengpeng.gao <tengpeng.gao@gmail.com>
2019-07-25 16:19:32 +0800

    初始化权限引擎

---
+++ flowable-action-guide/src/main/java/com/ithinksky/ch003/IdmEngineTest.java
@@ -0,0 +1,35 @@
+package com.ithinksky.ch003;
+
+import com.ithinksky.ch002.ProcessEngineTest;
+import org.flowable.idm.engine.IdmEngine;
+import org.flowable.idm.engine.IdmEngineConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * @author tengpeng.gao
+ */
+public class IdmEngineTest {
+
+    private IdmEngine idmEngine;
+
+    @Before
+    public void init() {
+        // 创建权限引擎
+        InputStream inputStream = ProcessEngineTest.class.getClassLoader()
+                .getResourceAsStream("ch003/flowable.idm.cfg.xml");
+        idmEngine = IdmEngineConfiguration.createIdmEngineConfigurationFromInputStream(inputStream).buildIdmEngine();
+        String name = idmEngine.getName();
+        System.out.println("引擎名称：" + name);
+
+    }
+
+    @Test
+    public void testInit() {
+        // 初次执行的时候会初始化生成 9 张 权限引擎相关的表
+    }
+
+
+}
